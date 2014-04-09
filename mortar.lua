414e9242afc1c3d96c434efa3cf9fc83b959765a
jessewmc <jessewmc@gmail.com>
2014-04-09 12:53:51 -0600

    modifying for winlose

--- mortar.lua
+++ mortar.lua
@@ -2,19 +2,21 @@ mortar = {}
 math.randomseed(os.time())
 
 function mortar.new()
-	return { x = player_sprite.x,
-			y = player_sprite.y,
+	local scr_x, scr_y = love.window.getDimensions()
+
+	return { x = scr_x + player_sprite.x - 150,
+			y = player_sprite.y - 150,
 			speed = 300,
 			accel_y = 5000,
-			velocity_y = -1500,
+			velocity_y = -150,
 			velocity_max = 10,
 			grounded = false,
 			jump = -1500,
 			color = {255, math.random(0,255), 0},
-			width = 25,
-			height = 25,
+			width = 50,
+			height = 50,
 			touching = false,
-			velocity_x = 100,
+			velocity_x = -200,
 			accel_x = 10}
 end
 
